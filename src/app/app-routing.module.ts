d56b81cb0c21997f7e56344b4e3232b056124e7a
nexus122 <juan-pablo122@hotmail.es>
2022-07-13 00:15:56 +0200

    Angular First Commit Scaffolding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeTestComponentComponent} from './components/home-test-component/home-test-component.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeTestComponentComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
